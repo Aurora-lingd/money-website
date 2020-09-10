908acb7bd4edddbfb4cfb45b3f1f3fbe349d86b3
tracy <843358687@qq.com>
2020-09-10 17:37:07 +0800

    deploy

---
+++ precache-manifest.13f67a4ea099d83d84682c5151529cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2caad2e56a0cb1b7b958",
+    "url": "/money-website/css/app.fa26e036.css"
+  },
+  {
+    "revision": "3e08644d16c51f6b57bfadf10d16f5ae",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "2caad2e56a0cb1b7b958",
+    "url": "/money-website/js/app.09dd777a.js"
+  },
+  {
+    "revision": "85539b37ab3270e8f029",
+    "url": "/money-website/js/chunk-vendors.34a4ec17.js"
+  },
+  {
+    "revision": "8743e3c6d3a9deb93a5a625838948152",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "7347eb99f9e0dda7db625784ec1f39d5",
+    "url": "/money-website/qrcord.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
